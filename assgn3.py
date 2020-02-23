0b9d1ab2941a2a095968980f54fbbd4389dded9b
chanchal231bansal <61386311+chanchal231bansal@users.noreply.github.com>
2020-02-23 23:01:05 +0530

    Add files via upload

---
+++ assgn3.py
@@ -0,0 +1,14 @@
+n=int(input())
+c=bin(n)
+c=c[2:]
+print(c,type(c))
+a=[]
+b=0
+for i in c:
+    if int(i)==0:
+        b=0
+    if int(i)==1:
+        b+=1
+    if b>0:
+        a.append(b)
+print(max(a))
